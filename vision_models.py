3ebb3563f05a2213a9a559b85b5688f5708cbb70
shaykrichelifrenn <skricheli2@gmail.com>
2023-10-03 15:48:58 -0700

    3.10 update

--- vision_models.py
+++ vision_models.py
@@ -12,10 +12,9 @@ import matplotlib.pyplot as plt
 from abc import ABC
 from pathlib import Path
 import sys
-import timm
 
-batch_size = 24
-lr = 1 * 10 ** (-4)
+batch_size = 16
+lr = 1 * 10 ** (-3)
 scheduler_gamma = 0.1
 num_epochs = 2
 cwd = Path(__file__).parent.resolve()
@@ -27,7 +26,7 @@ vit_model_names = {0: 'b_16',
                    3: 'l_32',
                    4: 'h_14'}
 
-vit_model_indices = [3]
+vit_model_indices = [2]
 train_folder_name = 'train'
 test_folder_name = 'test'
 
@@ -118,19 +117,6 @@ class FineTuner(torch.nn.Module, ABC):
         return sum(p.numel() for p in self.parameters())
 
 
-class InceptionV3FineTuner(FineTuner):
-    def __init__(self, num_classes: int):
-        super().__init__()
-        self.inception = torchvision.models.inception_v3(
-            weights=torchvision.models.Inception_V3_Weights.DEFAULT)
-        num_features = self.inception.fc.in_features
-        self.inception.fc = torch.nn.Linear(in_features=num_features, out_features=num_classes)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.inception(x)
-        return x
-
-
 class VITFineTuner(FineTuner):
     def __init__(self,
                  vit_model_index: int,
@@ -150,18 +136,6 @@ class VITFineTuner(FineTuner):
 
     def __str__(self):
         return f'{super().__str__()}_{self.vit_model_name}'
-
-
-class InceptionResNetV2FineTuner(FineTuner):
-    def __init__(self, num_classes: int):
-        super().__init__()
-        self.inception = timm.create_model('inception_resnet_v2',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.inception(x)
-        return x
 
 
 def test(fine_tuner: FineTuner) -> Tuple[list[int], list[int], float]:
@@ -230,21 +204,21 @@ def fine_tune(fine_tuner: FineTuner) -> Tuple[list[int], list[int], list[int], l
         train_predictions = []
         train_ground_truths = []
 
-        for i, data in tqdm(enumerate(train_loader, 0), total=len(train_loader)):
-            inputs, labels = data[0].to(device), data[1].to(device)
+        for i, X_Y in tqdm(enumerate(train_loader, 0), total=len(train_loader)):
+            X, Y = X_Y[0].to(device), X_Y[1].to(device)
             optimizer.zero_grad()
-            outputs = fine_tuner(inputs)
+            Y_pred = fine_tuner(X)
 
-            if type(outputs) == torchvision.models.InceptionOutputs:
-                outputs = outputs[0]
+            if type(Y_pred) == torchvision.models.InceptionOutputs:
+                Y_pred = Y_pred[0]
 
-            loss = criterion(outputs, labels)
+            loss = criterion(Y_pred, Y)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
 
-            predicted = torch.max(outputs, 1)[1]
-            train_ground_truths += labels.tolist()
+            predicted = torch.max(Y_pred, 1)[1]
+            train_ground_truths += Y.tolist()
             train_predictions += predicted.tolist()
 
         true_labels = np.array(train_ground_truths)
@@ -263,14 +237,14 @@ def fine_tune(fine_tuner: FineTuner) -> Tuple[list[int], list[int], list[int], l
         test_accuracies += [test_accuracy]
         print('#' * 100)
 
-
         colab_path = '/content/drive/My Drive/' if is_running_in_colab() else ''
 
         np.save(f"{colab_path}{fine_tuner}_train_acc.npy", train_accuracies)
         np.save(f"{colab_path}{fine_tuner}_train_loss.npy", train_losses)
 
         test_accuracies_filename = Path(f"{colab_path}{fine_tuner}_test_acc.npy")
-        if (not Path.is_file(test_accuracies_filename)) or (test_accuracies[-1] > np.load(test_accuracies_filename)[-1]):
+        if (not Path.is_file(test_accuracies_filename)) or (
+                test_accuracies[-1] > np.load(test_accuracies_filename)[-1]):
             np.save(test_accuracies_filename, test_accuracies)
 
         torch.save(fine_tuner.state_dict(), f"{fine_tuner}_lr{str(lr).split('.')[1]}_e{epoch}.pth")
@@ -286,9 +260,9 @@ if __name__ == '__main__':
                                                                                              model_name=model_name))
                 for model_name in model_names for train_or_val in [train_folder_name, test_folder_name]}
 
-    # assert all(datasets[f'{model_name}_{train_folder_name}'].classes ==
-    #            datasets[f'{model_name}_{test_folder_name}'].classes
-    #            for model_name in model_names)
+    assert all(datasets[f'{model_name}_{train_folder_name}'].classes ==
+               datasets[f'{model_name}_{test_folder_name}'].classes
+               for model_name in model_names)
 
     loaders = {f"{model_name}_{train_or_val}": torch.utils.data.DataLoader(
         dataset=datasets[f'{model_name}_{train_or_val}'],
@@ -303,10 +277,8 @@ if __name__ == '__main__':
     device = torch.device('mps' if torch.backends.mps.is_available() else
                           ("cuda" if torch.cuda.is_available() else 'cpu'))
 
-    fine_tuners = [eval(
-        f"{'VIT' if model_name.__contains__('vit') else 'Inception' if model_name == 'inception' else 'InceptionResNetV2'}"
-        f"FineTuner({'vit_model_index=model_index ,' if model_name.__contains__('vit') else ''}num_classes=n)")
-        for model_index, model_name in zip(vit_model_indices, model_names)]
+    fine_tuners = [VITFineTuner(vit_model_index=model_index, num_classes=n)
+                   for model_index, model_name in zip(vit_model_indices, model_names)]
 
     for fine_tuner in fine_tuners:
         with ClearCache(device):
