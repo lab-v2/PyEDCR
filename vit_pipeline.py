c7af938d6d960f26c8331fbf0b0fc5fcebfa8689
shaykrichelifrenn <skricheli2@gmail.com>
2023-11-27 19:50:48 -0700

    27.11 update

--- vit_pipeline.py
+++ vit_pipeline.py
@@ -15,7 +15,7 @@ batch_size = 32
 lrs = [1e-6]
 scheduler_gamma = 0.1
 num_epochs = 10
-vit_model_names = [f'vit_{vit_model_name}' for vit_model_name in ['b_16']]
+vit_model_names = [f'vit_{vit_model_name}' for vit_model_name in ['l_16']]
 
 cwd = pathlib.Path(__file__).parent.resolve()
 scheduler_step_size = num_epochs
