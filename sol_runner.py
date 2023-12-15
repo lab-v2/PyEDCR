2fab587924286ac3df9c43cfcd43f352da756a48
shaykrichelifrenn <skricheli2@gmail.com>
2023-12-14 18:26:28 -0700

    14.12 update

--- sol_runner.py
+++ sol_runner.py
@@ -3,5 +3,5 @@ import EDCR_pipeline
 
 
 def run():
-    vit_pipeline.run_individual_fine_tuning_pipeline()
-    # EDCR_pipeline.run_EDCR()
+    # vit_pipeline.run_individual_fine_tuning_pipeline()
+    EDCR_pipeline.run_EDCR_pipeline()
