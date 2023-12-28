3c5e952be7ac999d44d62eb529f3f9c86e2e68ef
shaykrichelifrenn <skricheli2@gmail.com>
2023-12-27 21:33:41 -0800

    27.12 update

--- EDCR_pipeline.py
+++ EDCR_pipeline.py
@@ -556,22 +556,22 @@ def run_EDCR_for_granularity(main_granularity: str,
                            get_unary_condition_values(example_index=example_index,
                                                       cla_datas=condition_datas['main']['fine_to_coarse'])
                    ) if conditions_from_main else [])
-                  # +
-                  # (
-                  #     ((get_binary_condition_values(example_index=example_index,
-                  #                                   fine_cla_datas=condition_datas['secondary']['fine'],
-                  #                                   coarse_cla_datas=condition_datas['secondary']['coarse'])
-                  #       if consistency_constraints else [])
-                  #      # +
-                  #      # get_unary_condition_values(example_index=example_index,
-                  #      #                            cla_datas=condition_datas['secondary']['fine']) +
-                  #      # get_unary_condition_values(example_index=example_index,
-                  #      #                            cla_datas=condition_datas['secondary']['coarse'])
-                  #      # +
-                  #      # get_unary_condition_values(example_index=example_index,
-                  #      #                            cla_datas=condition_datas['secondary']['fine_to_coarse'])
-                  #      )
-                  #     if conditions_from_secondary else [])
+                  +
+                  (
+                      ((get_binary_condition_values(example_index=example_index,
+                                                    fine_cla_datas=condition_datas['secondary']['fine'],
+                                                    coarse_cla_datas=condition_datas['secondary']['coarse'])
+                        if consistency_constraints else [])
+                       +
+                       get_unary_condition_values(example_index=example_index,
+                                                  cla_datas=condition_datas['secondary']['fine']) +
+                       get_unary_condition_values(example_index=example_index,
+                                                  cla_datas=condition_datas['secondary']['coarse'])
+                       +
+                       get_unary_condition_values(example_index=example_index,
+                                                  cla_datas=condition_datas['secondary']['fine_to_coarse'])
+                       )
+                      if conditions_from_secondary else [])
                   for example_index in range(examples_num)]
 
         all_charts = generate_chart(n_classes=len(classes),
--- sol_runner.py
+++ sol_runner.py
@@ -1,12 +1,14 @@
 import vit_pipeline
 import EDCR_pipeline
+import itertools
 
 
 def run():
     # vit_pipeline.run_individual_fine_tuning_pipeline()
 
     # vit_pipeline.run_combined_testing_pipeline(pretrained_path='vit_b_16_lr0.0001.pth')
-    EDCR_pipeline.run_EDCR_pipeline(combined=True,
-                                    conditions_from_secondary=True,
-                                    conditions_from_main=True,
-                                    consistency_constraints=True)
+    for a, b, c in itertools.product([True, False], repeat=3):
+        EDCR_pipeline.run_EDCR_pipeline(combined=True,
+                                        conditions_from_secondary=a,
+                                        conditions_from_main=b,
+                                        consistency_constraints=c)
