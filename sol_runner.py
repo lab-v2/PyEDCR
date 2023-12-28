93ee20914c15c103aa13658ba2ff2ba7bf9f5a3d
shaykrichelifrenn <skricheli2@gmail.com>
2023-12-27 22:19:38 -0800

    27.12 update

--- sol_runner.py
+++ sol_runner.py
@@ -1,14 +1,16 @@
 import vit_pipeline
 import EDCR_pipeline
 import itertools
+import utils
 
 
 def run():
     # vit_pipeline.run_individual_fine_tuning_pipeline()
-
     # vit_pipeline.run_combined_testing_pipeline(pretrained_path='vit_b_16_lr0.0001.pth')
+
     for a, b, c in itertools.product([True, False], repeat=3):
-        print(f'conditions_from_secondary={a}, conditions_from_main={b}, conditions_from_secondary={c}\n')
+        utils.red_text(f'\nconditions_from_secondary={a}, '
+                       f'conditions_from_main={b}, consistency_constraints={c}\n' + '#' * 100 + '\n')
 
         EDCR_pipeline.run_EDCR_pipeline(combined=True,
                                         conditions_from_secondary=a,
