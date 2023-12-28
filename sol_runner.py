5cab752980b435128fb48ba387d5f981155eddfb
shaykrichelifrenn <skricheli2@gmail.com>
2023-12-27 16:07:45 -0800

    27.12 update

--- sol_runner.py
+++ sol_runner.py
@@ -8,5 +8,5 @@ def run():
     # vit_pipeline.run_combined_testing_pipeline(pretrained_path='vit_b_16_lr0.0001.pth')
     EDCR_pipeline.run_EDCR_pipeline(combined=True,
                                     conditions_from_secondary=True,
-                                    conditions_from_main=False,
+                                    conditions_from_main=True,
                                     consistency_constraints=True)
