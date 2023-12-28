2ce4e8929cf5d7ab15a4f9e8dcece68604d2bdd1
shaykrichelifrenn <skricheli2@gmail.com>
2023-12-27 16:47:17 -0800

    27.12 update

--- EDCR_pipeline.py
+++ EDCR_pipeline.py
@@ -546,12 +546,12 @@ def run_EDCR_for_granularity(main_granularity: str,
                                                         fine_cla_datas=condition_datas['main']['fine'],
                                                         coarse_cla_datas=condition_datas['main']['coarse'])
                             if consistency_constraints else [])
-                           +
-                           get_unary_condition_values(example_index=example_index,
-                                                      cla_datas=condition_datas['main']['fine'])
-                           +
-                           get_unary_condition_values(example_index=example_index,
-                                                      cla_datas=condition_datas['main']['coarse'])
+                           # +
+                           # get_unary_condition_values(example_index=example_index,
+                           #                            cla_datas=condition_datas['main']['fine'])
+                           # +
+                           # get_unary_condition_values(example_index=example_index,
+                           #                            cla_datas=condition_datas['main']['coarse'])
                            # +
                            # get_unary_condition_values(example_index=example_index,
                            #                            cla_datas=condition_datas['main']['fine_to_coarse'])
@@ -562,11 +562,11 @@ def run_EDCR_for_granularity(main_granularity: str,
                                                     fine_cla_datas=condition_datas['secondary']['fine'],
                                                     coarse_cla_datas=condition_datas['secondary']['coarse'])
                         if consistency_constraints else [])
-                       +
-                       get_unary_condition_values(example_index=example_index,
-                                                  cla_datas=condition_datas['secondary']['fine']) +
-                       get_unary_condition_values(example_index=example_index,
-                                                  cla_datas=condition_datas['secondary']['coarse'])
+                       # +
+                       # get_unary_condition_values(example_index=example_index,
+                       #                            cla_datas=condition_datas['secondary']['fine']) +
+                       # get_unary_condition_values(example_index=example_index,
+                       #                            cla_datas=condition_datas['secondary']['coarse'])
                        # +
                        # get_unary_condition_values(example_index=example_index,
                        #                            cla_datas=condition_datas['secondary']['fine_to_coarse'])
