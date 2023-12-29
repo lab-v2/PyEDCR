7ca4780ed2651a2d2d175b4018c964d553cb761f
aniruddha123reinforcement <81187574+aniruddha123reinforcement@users.noreply.github.com>
2023-12-28 21:43:04 -0700

    Update vit_pipeline.py

--- vit_pipeline.py
+++ vit_pipeline.py
@@ -473,11 +473,21 @@ def fine_tune_combined_model(fine_tuner: models.FineTuner,
                             batch_total_loss = alpha * batch_fine_grain_loss + (1 - alpha) * batch_coarse_grain_loss
                         
                         elif loss_mode == "BCE loss":
-                            criterion = torch.nn.CrossEntropyLoss()
+                            criterion = torch.nn.BCEwithLogitsLoss()
+                            batch_total_loss = criterion(Y_pred, Y_combine)
+
+                        elif loss_mode == "soft marginal loss":
+                            criterion = torch.nn.MultiLabelSoftMarginLoss()
                             batch_total_loss = criterion(Y_pred, Y_combine)
                         
-                        elif loss_mode == "LTN loss":
-                            criterion = torch.nn.CrossEntropyLoss()
+                        elif loss_mode == "LTN BCE loss":
+                            criterion = torch.nn.BCEwithLogitsLoss()
+                            sat_agg = compute_sat_normally(logits_to_predicate,
+                                               Y_pred, Y_coarse_grain, Y_fine_grain)
+                            batch_total_loss = beta*(1. - sat_agg) + (1 - beta) * (criterion(Y_pred, Y_combine))
+
+                        elif loss_mode == "LTN Soft marginal loss":
+                            criterion = torch.nn.MultiLabelSoftMarginLoss()
                             sat_agg = compute_sat_normally(logits_to_predicate,
                                                Y_pred, Y_coarse_grain, Y_fine_grain)
                             batch_total_loss = beta*(1. - sat_agg) + (1 - beta) * (criterion(Y_pred, Y_combine))
