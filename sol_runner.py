9705253e1ac412014b7f96a7db8f9a347991a945
shaykrichelifrenn <skricheli2@gmail.com>
2023-12-28 18:07:54 -0800

    28.12 update

--- sol_runner.py
+++ sol_runner.py
@@ -9,8 +9,7 @@ def run():
     # vit_pipeline.run_combined_testing_pipeline(pretrained_path='vit_b_16_lr0.0001.pth')
 
     for a, b in itertools.product([True, False], repeat=2):
-        print(utils.red_text(f'\nconditions_from_secondary={a}, '
-                             f'conditions_from_main={b}\n' +
+        print(utils.red_text(f'\nconditions_from_secondary={a}, conditions_from_main={b}\n' +
                              '#' * 100 + '\n'))
 
         EDCR_pipeline.run_EDCR_pipeline(combined=True,
