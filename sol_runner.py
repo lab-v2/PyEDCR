e2c5cdcabdd156412aa2a5ea0292721a07af8f4d
shaykrichelifrenn <skricheli2@gmail.com>
2023-12-29 12:23:03 -0800

    29.12 update

--- sol_runner.py
+++ sol_runner.py
@@ -6,13 +6,16 @@ import utils
 
 def run():
     # vit_pipeline.run_individual_fine_tuning_pipeline()
-    # vit_pipeline.run_combined_testing_pipeline(pretrained_path='vit_b_16_lr0.0001.pth')
-    # vit_pipeline.run_combined_fine_tuning_pipeline()
-    for a, b in itertools.product([True, False], repeat=2):
-        print(utils.red_text(f'\nconditions_from_secondary={a}, conditions_from_main={b}\n' +
-                             '#' * 100 + '\n'))
 
-        EDCR_pipeline.run_EDCR_pipeline(combined=False,
-                                        conditions_from_secondary=a,
-                                        conditions_from_main=b,
-                                        consistency_constraints=True)
+    # vit_pipeline.run_combined_testing_pipeline(pretrained_path='vit_b_16_lr0.0001.pth')
+
+    vit_pipeline.run_combined_fine_tuning_pipeline()
+
+    # for a, b in itertools.product([True, False], repeat=2):
+    #     print(utils.red_text(f'\nconditions_from_secondary={a}, conditions_from_main={b}\n' +
+    #                          '#' * 100 + '\n'))
+    #
+    #     EDCR_pipeline.run_EDCR_pipeline(combined=False,
+    #                                     conditions_from_secondary=a,
+    #                                     conditions_from_main=b,
+    #                                     consistency_constraints=True)
