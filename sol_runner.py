1063ccfd9424428ba3898b3ff7241c29d1eae227
shaykrichelifrenn <skricheli2@gmail.com>
2024-01-09 20:53:42 -0700

    9.1 update

--- sol_runner.py
+++ sol_runner.py
@@ -5,7 +5,7 @@ import utils
 
 
 def run():
-    vit_pipeline.run_combined_fine_tuning_pipeline(lrs=[0.0001],
+    vit_pipeline.run_combined_fine_tuning_pipeline(lrs=[5e-5],
                                                    loss='BCE')
     # vit_pipeline.run_individual_fine_tuning_pipeline()
 
