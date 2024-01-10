c10bd3d6d75bd7479677ca2ecf92bd3de7ba3a5b
shaykrichelifrenn <skricheli2@gmail.com>
2024-01-10 01:03:15 -0700

    10.1 update

--- EDCR_pipeline.py
+++ EDCR_pipeline.py
@@ -19,7 +19,7 @@ figs_folder = 'figs/'
 results_file = "rule_for_NPcorrection.csv"
 
 main_model_name = 'vit_b_16'
-main_lr = 0.0001
+main_lr = 3e-6
 epochs_num = 20
 
 secondary_model_name = 'vit_l_16'
