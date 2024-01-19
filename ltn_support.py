4375723155e12f4f5ff97627f6c28008c92ee188
shaykrichelifrenn <skricheli2@gmail.com>
2024-01-18 22:17:36 -0700

    updated ltn_support

--- ltn_support.py
+++ ltn_support.py
@@ -92,9 +92,16 @@ def compute_sat_normally(logits_to_predicate,
 
     # Rewrite the inconsistency code (Forall(x, Implies(P(x,coarse_label), Not(P(x,coarse_to_not_fine))))
     for i in coarse_label_dict.values():
-        for j in coarse_to_not_fine.values():
-            sat_agg_list.append(
-                Forall(x_variables[i],Implies(logits_to_predicate(x,l[i]),Not(x,l_c[j]))))
+        for j in fine_label_dict.values():
+            corresponding_coarse_label = data_preprocessing.fine_to_course_idx[j] + len(fine_label_dict)
+            if (corresponding_coarse_label != i):
+                sat_agg_list.append(
+                    Forall(x,
+                           Implies(logits_to_predicate(x,l[i]), 
+                                   Not(logits_to_predicate(x,l[j]))
+                                   )
+                            )
+                )
     # Fine labels: for all x[i], x[i] -> l[i]
 
     for i in fine_label_dict.values():
@@ -114,3 +121,102 @@ def compute_sat_normally(logits_to_predicate,
     )
 
     return sat_agg
+
+def compute_sat_testing_value(logits_to_predicate,
+                         prediction, labels_coarse, labels_fine):
+    """
+    compute satagg function for rules
+    argument:
+      - logits_to_predicate: get the satisfaction of a variable given the label
+      - prediction: output of fine tuner, 
+      - labels_coarse, labels_fine: ground truth of coarse and fine label
+      - fine_to_coarse: dictionary mapping fine-grain class to coarse-grain class
+
+    return:
+      sat_agg_label: list contain rules and its confidence value
+
+    """
+    Not = ltn.Connective(ltn.fuzzy_ops.NotStandard())
+    And = ltn.Connective(ltn.fuzzy_ops.AndProd())
+    Implies = ltn.Connective(ltn.fuzzy_ops.ImpliesReichenbach())
+    Forall = ltn.Quantifier(
+        ltn.fuzzy_ops.AggregPMeanError(p=4), quantifier="f")
+    SatAgg = ltn.fuzzy_ops.SatAgg()
+
+    fine_label_dict = {name: label for label, name in enumerate(data_preprocessing.fine_grain_classes)}
+    coarse_label_dict = {name: label + len(data_preprocessing.fine_grain_classes) for label, name in enumerate(data_preprocessing.coarse_grain_classes)}
+    labels_fine = labels_fine.detach().to('cpu')
+    labels_coarse = labels_coarse.detach().to('cpu') + len(data_preprocessing.fine_grain_classes)
+        
+    # Define constant
+    l = {}
+    num_labels = len(data_preprocessing.fine_grain_classes) + len(data_preprocessing.coarse_grain_classes)
+    for label in range(num_labels):
+        one_hot = torch.zeros(num_labels)
+        one_hot[label] = 1.0
+        l[label] = ltn.Constant(one_hot, trainable=True)
+
+    # Define variables
+    x_variables = {}
+    x = ltn.Variable("x", prediction)
+
+    for name, label in fine_label_dict.items():
+        x_variables[label] = ltn.Variable(
+            name, prediction[labels_fine == label])
+    for name, label in coarse_label_dict.items():
+        x_variables[label] = ltn.Variable(
+            name, prediction[labels_coarse == label])
+
+    sat_agg_label = []
+
+    # Rewrite the inconsistency code (Forall(x, Implies(P(x,coarse_label), Not(P(x,coarse_to_not_fine))))
+    for coarse_label, i in coarse_label_dict.items():
+        for fine_label, j in fine_label_dict.items():
+            corresponding_coarse_label = data_preprocessing.fine_to_course_idx[j] + len(fine_label_dict)
+            if (corresponding_coarse_label != i):
+                satisfaction = Forall(x,
+                                      Implies(logits_to_predicate(x,l[i]), 
+                                              Not(logits_to_predicate(x,l[j]))
+                                      )
+                )
+                sat_agg_label.append([0, 
+                                      f"for all x, P(x, l[{coarse_label}]) imply -P(x, l[{fine_label}])", 
+                                      satisfaction.value.detach().item()])
+                
+    # Coarse labels: for all x[i], x[i] -> l[i]
+
+    for coarse_label, i in coarse_label_dict.items():
+        satisfaction = Forall(x_variables[i], logits_to_predicate(x_variables[i], l[i]))
+        sat_agg_label.append([1, 
+                              f'for all {coarse_label}, P(x[{coarse_label}], l[{coarse_label}])', 
+                              satisfaction.value.detach().item()])
+
+    # Coarse Label: for all x, - (P(x, l[coarse_1] and x[different coarse]}
+
+    for coarse_label_1, i in coarse_label_dict.items():
+        for coarse_label_2, j in coarse_label_dict.items():
+            if i != j :
+                satisfaction = Forall(x, Not(And(logits_to_predicate(x, l[i]), logits_to_predicate(x, l[j]))))
+                sat_agg_label.append([2, 
+                                      f"for all x, - (P(x, {coarse_label_1}) and P(x,{coarse_label_2}))", 
+                                      satisfaction.value.detach().item()])
+
+    # Fine labels: for all x[i], x[i] -> l[i]
+
+    for fine_label, i in fine_label_dict.items():
+        satisfaction = Forall(x_variables[i], logits_to_predicate(x_variables[i], l[i]))
+        sat_agg_label.append([1, 
+                              f'for all {fine_label}, P(x[{fine_label}], l[{fine_label}])', 
+                              satisfaction.value.detach().item()])
+    # Fine Label: for all x[fine], - {x[fine] and x[different fine]}
+
+    for fine_label_1, i in fine_label_dict.items():
+        for fine_label_2, j in fine_label_dict.items():
+            if i != j :
+                satisfaction = Forall(x, Not(And(logits_to_predicate(x, l[i]), logits_to_predicate(x, l[j]))))
+                sat_agg_label.append([2, 
+                                      f"for all x, - (P(x, {fine_label_1}) and P(x,{fine_label_2}))", 
+                                      satisfaction.value.detach().item()])
+
+
+    return sat_agg_label
