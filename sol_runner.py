0c9528bc48651708e1555aaaf0779b825271c90b
SpencerOzgur <sozgur@asu.edu>
2024-01-27 16:28:47 -0700

    1/27

--- sol_runner.py
+++ sol_runner.py
@@ -8,16 +8,21 @@ def run():
     lrs = [0.0001, 3e-06]
     losses = ['BCE', 'soft_marginal']
     from_main = [True, False]
+    combined = [False, True]
 
-    for main in from_main:
-        for mult in mults:
-            for loss in losses:
-                for lr in lrs:
-                    EDCR_pipeline.run_EDCR_pipeline(
-                        main_lr=lr,
-                        combined=True,
-                        loss=loss,
-                        conditions_from_secondary=not main,
-                        conditions_from_main=main,
-                        consistency_constraints=True,
-                        multiprocessing=mult)
+    for comb in combined:
+        for main in from_main:
+            for mult in mults:
+                for loss in losses:
+                    for lr in lrs:
+                        if not comb and lr == 3e-06:
+                            continue
+
+                        EDCR_pipeline.run_EDCR_pipeline(
+                            main_lr=lr,
+                            combined=comb,
+                            loss=loss,
+                            conditions_from_secondary=not main,
+                            conditions_from_main=main,
+                            consistency_constraints=True,
+                            multiprocessing=mult)
