62d6568df9cc4654a05282f7200a9671f8f954bd
SpencerOzgur <sozgur@asu.edu>
2024-01-29 14:09:13 -0700

    1/29

--- sol_runner.py
+++ sol_runner.py
@@ -4,35 +4,56 @@ import utils
 
 
 def run():
-    vit_pipeline.run_combined_fine_tuning_pipeline(lrs=[0.0001],
-                                                   loss='soft_marginal')
-    # vit_pipeline.run_individual_fine_tuning_pipeline()
+    EDCR_pipeline.run_EDCR_pipeline(
+        main_lr=1e-06,
+        combined=False,
+        loss='BCE',
+        conditions_from_secondary=True,
+        conditions_from_main=False,
+        consistency_constraints=False,
+        multiprocessing=True)
 
-    # vit_pipeline.run_combined_testing_pipeline(pretrained_path='vit_b_16_lr0.0001.pth')
+    EDCR_pipeline.run_EDCR_pipeline(
+        main_lr=0.0001,
+        combined=False,
+        loss='BCE',
+        conditions_from_secondary=True,
+        conditions_from_main=False,
+        consistency_constraints=False,
+        multiprocessing=True)
 
-    # vit_pipeline.run_individual_fine_tuning_pipeline()
+    EDCR_pipeline.run_EDCR_pipeline(
+        main_lr=3e-06,
+        combined=True,
+        loss='BCE',
+        conditions_from_secondary=True,
+        conditions_from_main=False,
+        consistency_constraints=False,
+        multiprocessing=True)
 
-    # combined = True
-    # losses = ['BCE', 'soft_marginal']
-    # lrs = [3e-6]
-    #
-    # for main_lr in lrs:
-    #     # for loss in losses:
-    #     print('\n')
-    #     for i in range(3):
-    #         print(utils.red_text('#' * 150))
-    #     print('\n')
-    #
-    #     for conditions_from_main in [True, False]:
-    #
-    #         print(utils.red_text(f'\nconditions_from_secondary={not conditions_from_main}, '
-    #                              f'conditions_from_main={conditions_from_main}\n' +
-    #                              f'combined={combined}\n' + '#' * 100 + '\n'))
-    #
-    #         EDCR_pipeline.run_EDCR_pipeline(main_lr=main_lr,
-    #                                         combined=combined,
-    #                                         loss=losses[0],
-    #                                         conditions_from_secondary=not conditions_from_main,
-    #                                         conditions_from_main=conditions_from_main,
-    #                                         consistency_constraints=True,
-    #                                         multiprocessing=True)
+    EDCR_pipeline.run_EDCR_pipeline(
+        main_lr=0.0001,
+        combined=True,
+        loss='BCE',
+        conditions_from_secondary=True,
+        conditions_from_main=False,
+        consistency_constraints=False,
+        multiprocessing=True)
+
+    EDCR_pipeline.run_EDCR_pipeline(
+        main_lr=3e-06,
+        combined=True,
+        loss='soft_marginal',
+        conditions_from_secondary=True,
+        conditions_from_main=False,
+        consistency_constraints=False,
+        multiprocessing=True)
+
+    EDCR_pipeline.run_EDCR_pipeline(
+        main_lr=0.0001,
+        combined=True,
+        loss='soft_marginal',
+        conditions_from_secondary=True,
+        conditions_from_main=False,
+        consistency_constraints=False,
+        multiprocessing=True)
