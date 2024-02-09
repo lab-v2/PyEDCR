3daf988584bcdba2901f7641e8fa2302b1c3fa0b
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-02-08 19:26:31 -0700

    add docstring to PyEDCR

--- PyEDCR.py
+++ PyEDCR.py
@@ -10,19 +10,48 @@ import context_handlers
 
 
 class Condition:
+    """Represents a condition that can be evaluated on examples.
+
+    When treated as a function, it takes an example (e.g., image, data) as input
+    and returns a value between 0 and 1 indicating whether the condition is satisfied.
+    A value of 0 means the condition is not met, while a value of 1 means it is fully met.
+    """
+
     def __init__(self,
                  data: np.array):
+        """Initializes a Condition instance.
+
+        Args:
+            data: A NumPy array representing the condition data.
+        """
         self.__data = data
 
     @property
     def data(self):
+        """Gets the condition data.
+
+        Returns:
+            numpy.ndarray: The condition data.
+        """
         return self.__data
 
 
 class PredCondition(Condition):
+    """Represents a condition based on a model's prediction of a specific class.
+
+    It evaluates to 1 if the model predicts the specified class for a given example,
+    and 0 otherwise.
+    """
+
     def __init__(self,
                  pred_data: np.array,
                  l: data_preprocessing.Label):
+        """Initializes a PredCondition instance.
+
+        Args:
+            pred_data: A NumPy array containing class predictions for examples.
+            l: The target Label for which the condition is evaluated.
+        """
         super().__init__(data=np.where(pred_data == l.index, 1, 0))
         self.__l = l
 
@@ -32,6 +61,22 @@ class Rule:
 
 
 class EDCR:
+    """Performs error detection and correction based on model predictions.
+
+    This class aims to identify and rectify errors in predictions made by a
+    specified neural network model. It utilizes prediction data from both
+    fine-grained and coarse-grained model runs to enhance its accuracy.
+
+    Attributes:
+        __main_model_name (str): Name of the primary model used for predictions.
+        __combined (bool): Whether combined features (coarse and fine) were used during training.
+        __loss (str): Loss function used during training.
+        __lr: Learning rate used during training.
+        __num_epochs (int): Number of training epochs.
+        __epsilon: Value using for constraint in getting rules
+        __rules: ...
+
+    """
     def __init__(self,
                  main_model_name: str,
                  combined: bool,
@@ -109,6 +154,12 @@ class EDCR:
 
     def __get_predictions(self,
                           test: bool) -> (np.array, np.array):
+        """Retrieves prediction data based on specified test/train mode.
+
+        :param test: True for test data, False for training data.
+        :return: Fine-grained and coarse-grained prediction data.
+        """
+        
         pred_fine_data, pred_coarse_data = [self.__test_pred_data[g] for g in data_preprocessing.granularities] \
             if test else [self.__train_pred_data[g] for g in data_preprocessing.granularities]
 
@@ -116,7 +167,15 @@ class EDCR:
 
     def print_metrics(self,
                       test: bool):
+        
+        """Prints performance metrics for given test/train data.
 
+        Calculates and prints various metrics (accuracy, precision, recall, etc.)
+        using appropriate true labels and prediction data based on the specified mode.
+
+        :param test: True to use test data, False to use training data.
+        :return: None
+        """
         pred_fine_data, pred_coarse_data = self.__get_predictions(test=test)
         true_fine_data, true_coarse_data = data_preprocessing.get_ground_truths(test=test)
 
@@ -133,21 +192,44 @@ class EDCR:
     @staticmethod
     def __get_corr_l(g: data_preprocessing.Granularity,
                      l: data_preprocessing.Label):
+        """Calculates correct label mask for given granularity and label.
+
+        :param g: The granularity level 
+        :param l: The label of interest.
+        :return: A mask with 1s for correct labels, 0s otherwise.
+        """
         return np.where(data_preprocessing.get_ground_truths(test=False, g=g) == l.index, 1, 0)
 
     def __get_tp_l(self,
                    g: data_preprocessing.Granularity,
                    l: data_preprocessing.Label):
+        """Calculates true positive mask for given granularity and label.
+            
+        :param g: The granularity level.
+        :param l: The label of interest.
+        :return: A mask with 1s for true positive instances, 0s otherwise.
+        """
         return np.where(self.__train_pred_data[g] == data_preprocessing.get_ground_truths(test=False, g=g) &
                         (self.__train_pred_data[g] == l.index), 1, 0)
 
     def __get_fp_l(self,
                    g: data_preprocessing.Granularity,
                    l: data_preprocessing.Label):
+        """Calculates false positive mask for given granularity and label.
+
+        :param g: The granularity level
+        :param l: The label of interest.
+        :return: A mask with 1s for false positive instances, 0s otherwise.
+        """
         return np.where((self.__train_pred_data[g] == l.index) &
                         (data_preprocessing.get_ground_truths(test=False, g=g) != l.index), 1, 0)
 
     def __are_all_conditions_satisfied(self, C: set[Condition]):
+        """Checks if all given conditions are satisfied for each example.
+        
+        :param C: A set of `Condition` objects.
+        :return: A NumPy array with True values if the example satisfy all conditions and False otherwise.
+        """
         all_conditions_satisfied = np.ones_like(self.__train_pred_data[data_preprocessing.granularities[0]])
 
         for cond in C:
@@ -159,6 +241,13 @@ class EDCR:
                     g: data_preprocessing.Granularity,
                     l: data_preprocessing.Label,
                     C: set[Condition]) -> int:
+        """Calculate the number of samples that satisfy the conditions for some set of condition and have true positive.
+        
+        :param C: A set of `Condition` objects.
+        :param g: The granularity level
+        :param l: The label of interest.
+        :return: The number of instances that is true negative and satisfying all conditions.
+        """
         tp_indices = self.__get_tp_l(g=g, l=l)
         all_conditions_satisfied = self.__are_all_conditions_satisfied(C)
         NEG_l = int(np.sum(tp_indices * all_conditions_satisfied))
@@ -169,6 +258,13 @@ class EDCR:
                     g: data_preprocessing.Granularity,
                     l: data_preprocessing.Label,
                     C: set[Condition]) -> int:
+        """Calculate the number of samples that satisfy the conditions for some set of condition and have false positive.
+        
+        :param C: A set of `Condition` objects.
+        :param g: The granularity level
+        :param l: The label of interest.
+        :return: The number of instances that is false negative and satisfying all conditions.
+        """
         fp_indices = self.__get_fp_l(g=g, l=l)
         all_conditions_satisfied = self.__are_all_conditions_satisfied(C)
         POS_l = int(np.sum(fp_indices * all_conditions_satisfied))
@@ -179,6 +275,14 @@ class EDCR:
                     g: data_preprocessing.Granularity,
                     l: data_preprocessing.Label,
                     CC: set[(Condition, data_preprocessing.Label)]) -> float:
+        """Calculate the ratio of number of samples that satisfy the rule body and head with the ones that only satisfy the body,
+        given a condition class pair.
+        
+        :param C: A set of `Condition` objects.
+        :param g: The granularity level
+        :param l: The label of interest.
+        :return: ratio as defined above
+        """
         corr_l = self.__get_corr_l(g=g, l=l)
 
         any_pairs_satisfied = 0
@@ -198,6 +302,13 @@ class EDCR:
     def __DetRuleLearn(self,
                        g: data_preprocessing.Granularity,
                        l: data_preprocessing.Label) -> set[Condition]:
+        """Learns error detection rules for a specific label and granularity. These rules capture conditions
+        that, when satisfied, indicate a higher likelihood of prediction errors for a given label.
+
+        :param g: The granularity level.
+        :param l: The label of interest.
+        :return: A set of `Condition` representing the learned error detection rules.
+        """
         DC_l = set()
 
         N_l = np.sum(np.where(self.__train_pred_data[g] == l.index, 1, 0))
@@ -232,6 +343,14 @@ class EDCR:
                         l: data_preprocessing.Label,
                         CC_all: set[(Condition, data_preprocessing.Label)]) -> \
             set[tuple[Condition, data_preprocessing.Label]]:
+        """Learns error correction rules for a specific label and granularity. These rules associate conditions with alternative
+        labels that are more likely to be correct when those conditions are met.
+
+        :param g: The granularity level (e.g., 'fine', 'coarse').
+        :param l: The label of interest.
+        :param CC_all: A set of all condition-label pairs to consider for rule learning.
+        :return: A set of condition-label pairs.
+        """
         CC_l = set()
         CC_l_prime = CC_all
 
@@ -254,6 +373,10 @@ class EDCR:
 
     def DetCorrRuleLearn(self,
                          g: data_preprocessing.Granularity):
+        """Learns error detection and correction rules for all labels at a given granularity.
+
+        :param g: The granularity level (e.g., 'fine', 'coarse').
+        """
         CC_all = set()
         granularity_labels = data_preprocessing.get_labels(g)
 
