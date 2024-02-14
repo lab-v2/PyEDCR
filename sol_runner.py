cc10b97d13ec8b94431ea82698e34b990294b5a7
SpencerOzgur <sozgur@asu.edu>
2024-02-13 17:06:53 -0700

    2/13

--- sol_runner.py
+++ sol_runner.py
@@ -10,56 +10,22 @@ def run():
 
     # vit_pipeline.run_combined_testing_pipeline(pretrained_path='vit_b_16_lr0.0001.pth')
 
-    # combined = True
-    # losses = ['BCE', 'soft_marginal']
-    # lrs = [0.0001, 3e-6]
-    #
-    # for loss in losses:
-    #     for main_lr in lrs:
-    #         print('\n')
-    #         for i in range(3):
-    #             print(utils.red_text('#' * 150))
-    #         print('\n')
-    #
-    #         print(utils.red_text(f'combined={combined}\n' + '#' * 100 + '\n'))
-    #
-    #         EDCR_pipeline.run_EDCR_pipeline(main_lr=main_lr,
-    #                                         combined=combined,
-    #                                         loss=loss,
-    #                                         consistency_constraints=True,
-    #                                         multiprocessing=True)
 
     combined = True
-    print(utils.red_text(f'combined={combined}\n' + '#' * 100 + '\n'))
+    losses = ['BCE', 'soft_marginal']
+    lrs = [0.0001, 3e-6]
 
-    test_pred_fine_path = 'combined_results/vit_b_16_test_fine_pred_lr0.0001_e19.npy'
-    test_pred_coarse_path = 'combined_results/vit_b_16_test_coarse_pred_lr0.0001_e19.npy'
+    for loss in losses:
+        for main_lr in lrs:
+            print('\n')
+            for i in range(3):
+                print(utils.red_text('#' * 150))
+            print('\n')
 
-    test_true_fine_path = 'combined_results/test_true_fine.npy'
-    test_true_coarse_path = 'combined_results/test_true_coarse.npy'
+            print(utils.red_text(f'combined={combined}\n' + '#' * 100 + '\n'))
 
-    train_pred_fine_path = 'combined_results/train_vit_b_16_fine_pred_lr0.0001.npy'
-    train_pred_coarse_path = 'combined_results/train_vit_b_16_coarse_pred_lr0.0001.npy'
-
-    train_true_fine_path = 'combined_results/train_true_fine.npy'
-    train_true_coarse_path = 'combined_results/train_true_coarse.npy'
-
-    epsilons = [0.2, 0.3, 0.7]
-
-    consistency_constraints = True
-    for epsilon in epsilons:
-        print(utils.red_text('\n' + '#' * 50 + f'Epsilon: {epsilon}' + '#' * 50 + '\n'))
-        EDCR_pipeline.run_EDCR_pipeline(test_pred_fine_path=test_pred_fine_path,
-                                        test_pred_coarse_path=test_pred_coarse_path,
-                                        test_true_fine_path=test_true_fine_path,
-                                        test_true_coarse_path=test_true_coarse_path,
-                                        train_pred_fine_path=train_pred_fine_path,
-                                        train_pred_coarse_path=train_pred_coarse_path,
-                                        train_true_fine_path=train_true_fine_path,
-                                        train_true_coarse_path=train_true_coarse_path,
-                                        main_lr=0.0001,
-                                        combined=combined,
-                                        loss='BCE',
-                                        epsilon=epsilon,
-                                        consistency_constraints=consistency_constraints,
-                                        multiprocessing=True)
+            EDCR_pipeline.run_EDCR_pipeline(main_lr=main_lr,
+                                            combined=combined,
+                                            loss=loss,
+                                            consistency_constraints=True,
+                                            multiprocessing=True)
