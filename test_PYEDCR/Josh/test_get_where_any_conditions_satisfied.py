5a8b8e78bbe87d31aaa27216b5b32e6e97ccf33d
shaykrichelifrenn <skricheli2@gmail.com>
2024-02-16 17:16:05 -0700

    16.2 update

--- test_PYEDCR/Josh/test_get_where_any_conditions_satisfied.py
+++ test_PYEDCR/Josh/test_get_where_any_conditions_satisfied.py
@@ -6,76 +6,66 @@ class TestWhereAnyConditionsSatisfied(Test):
                  epsilon: float,
                  K_train: list[(int, int)] = None,
                  K_test: list[(int, int)] = None):
-        super().__init__(epsilon, K_train, K_test)
+        method_str = 'get_where_any_conditions_satisfied'
+        super().__init__(epsilon, method_str, K_train, K_test)
 
-    def run(self,
-            method_str: str,
-            expected_output,
-            *method_args,
-            **method_kwargs
-            ):
-        pass
+    def run_edge_cases(self,
+                       fine_data: np.array,
+                       coarse_data: np.array):
+        self.run(C=set(),
+                 fine_data=fine_data,
+                 coarse_data=coarse_data,
+                 expected_output=0)
+
+        self.run(C={pred_conditions[l] for l in fg_l},
+                 fine_data=fine_data,
+                 coarse_data=coarse_data,
+                 expected_output=1)
+
+        self.run(C={pred_conditions[l] for l in cg_l},
+                 fine_data=fine_data,
+                 coarse_data=coarse_data,
+                 expected_output=1)
+
+        self.run(C={pred_conditions[l] for l in fg_l + cg_l},
+                 fine_data=fine_data,
+                 coarse_data=coarse_data,
+                 expected_output=1)
 
 
 def run_tests():
     K = [(0, 9)]
-    test = Test(epsilon=0.1, K_train=K, K_test=K)
+    test = TestWhereAnyConditionsSatisfied(epsilon=0.1, K_train=K, K_test=K)
 
     train_pred_fine_data, train_pred_coarse_data = test.edcr.get_predictions(test=False)
 
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C=set(),
+
+    test.run(C={pred_Tornado, pred_BMP_1},
              fine_data=train_pred_fine_data,
              coarse_data=train_pred_coarse_data,
              expected_output=0)
 
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C={pred_Tornado, pred_BMP_1},
-             fine_data=train_pred_fine_data,
-             coarse_data=train_pred_coarse_data,
-             expected_output=0)
-
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C={pred_Tornado, pred_BMP_1, pred_2S19_MSTA},
+    test.run(C={pred_Tornado, pred_BMP_1, pred_2S19_MSTA},
              fine_data=train_pred_fine_data,
              coarse_data=train_pred_coarse_data,
              expected_output=np.array([1] * 6 + [0] + [1] * 3))
 
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C={pred_SPA},
+    test.run(C={pred_SPA},
              fine_data=train_pred_fine_data,
              coarse_data=train_pred_coarse_data,
              expected_output=1)
 
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C={pred_Tornado, pred_BMP_1, pred_SPA},
+    test.run(C={pred_Tornado, pred_BMP_1, pred_SPA},
              fine_data=train_pred_fine_data,
              coarse_data=train_pred_coarse_data,
              expected_output=1)
 
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C={pred_2S19_MSTA, pred_T_72},
+    test.run(C={pred_2S19_MSTA, pred_T_72},
              fine_data=train_pred_fine_data,
              coarse_data=train_pred_coarse_data,
              expected_output=1)
 
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C={pred_conditions[l] for l in fg_l},
-             fine_data=train_pred_fine_data,
-             coarse_data=train_pred_coarse_data,
-             expected_output=1)
 
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C={pred_conditions[l] for l in cg_l},
-             fine_data=train_pred_fine_data,
-             coarse_data=train_pred_coarse_data,
-             expected_output=1)
-
-    test.run(method_str='get_where_any_conditions_satisfied',
-             C={pred_conditions[l] for l in fg_l + cg_l},
-             fine_data=train_pred_fine_data,
-             coarse_data=train_pred_coarse_data,
-             expected_output=1)
 
     # K = 12
     # edcr = EDCR.test(epsilon=0.1, K=K)
