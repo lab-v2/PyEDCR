ee5c9ae48263422498c3e80fdea07fce5e3193d4
SpencerOzgur <sozgur@asu.edu>
2024-02-26 17:16:51 -0700

    test_get_l_correction_rule_theoretical_precision_increase added

--- test_PYEDCR/Spencer/test_get_l_correction_rule_theoretical_precision_increase.py
+++ test_PYEDCR/Spencer/test_get_l_correction_rule_theoretical_precision_increase.py
@@ -6,7 +6,7 @@ def run_test_1():
     K = [(0, 9)]
     test = Test(epsilon=0.1, K_train=K, K_test=None, method_str=method_str)
 
-    test.edcr.set_error_correction_rules({l_BTR_80: {(pred_BTR_80, l_BTR_80)},
+    test.edcr.set_error_correction_rules({l_BTR_80: {(pred_BTR_80, l_BTR)},
                                           l_2S19_MSTA: {(pred_2S19_MSTA, l_SPA), (pred_T_72, l_SPA)}
                                           })
 
@@ -16,7 +16,19 @@ def run_test_1():
     test.run(l=l_2S19_MSTA, expected_output=0)
 
 def run_test_2():
+    K = [(2975, 2979), (1455, 1459), (3000, 3000), (3190, 3191), (3010, 3012)]
+    test = Test(epsilon=0.1, K_train=K, K_test=None, method_str=method_str)
+
+    test.edcr.set_error_correction_rules({l_BTR_80: {(pred_BTR_80, l_BTR)},
+                                          l_2S19_MSTA: {(pred_2S19_MSTA, l_SPA), (pred_T_72, l_SPA)}
+                                          })
+
+    test.run(l=l_BTR_80, expected_output=1/15)
+
+    test.print_examples(test=False)
+
 
 
 if __name__ == '__main__':
     run_test_1()
+    run_test_2()
