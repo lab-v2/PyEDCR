15367daedc0d9b5081fa6816d5508b3b2d709f33
shaykrichelifrenn <skricheli2@gmail.com>
2024-03-12 16:14:44 -0700

    12.3 update

--- vit_pipeline.py
+++ vit_pipeline.py
@@ -12,7 +12,7 @@ import data_preprocessing
 
 batch_size = 32
 scheduler_gamma = 0.9
-num_epochs = 1
+num_epochs = 5
 ltn_num_epochs = 5
 vit_model_names = [f'vit_{vit_model_name}' for vit_model_name in ['b_16']]
 
