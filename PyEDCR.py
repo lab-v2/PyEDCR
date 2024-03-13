8280272d7b27551dff4ed3383b4629df0fbc348b
shaykrichelifrenn <skricheli2@gmail.com>
2024-03-13 14:01:40 -0700

    13.3 update

--- PyEDCR.py
+++ PyEDCR.py
@@ -1440,7 +1440,7 @@ class EDCR:
 
         with (context_handlers.ClearSession()):
             vit_pipeline.fine_tune_combined_model(
-                lrs=[2 * self.lr],
+                lrs=[10 * self.lr],
                 fine_tuner=fine_tuners[0] if self.correction_model is None else self.correction_model,
                 device=devices[0],
                 loaders=loaders,
@@ -1463,7 +1463,7 @@ class EDCR:
             self.correction_model = fine_tuners[0]
 
         fine_tuners, loaders, devices, num_fine_grain_classes, num_coarse_grain_classes = vit_pipeline.initiate(
-            lrs=[self.lr],
+            lrs=[10 * self.lr],
             combined=self.combined,
             debug=False,
             indices=examples_with_errors,
