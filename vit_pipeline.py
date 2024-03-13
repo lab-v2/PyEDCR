ac1abbba8ab600fc2ae8c17969f4afc51b675e67
shaykrichelifrenn <skricheli2@gmail.com>
2024-03-13 12:54:58 -0700

    13.3 update

--- vit_pipeline.py
+++ vit_pipeline.py
@@ -1046,7 +1046,7 @@ if __name__ == '__main__':
     # run_combined_fine_tuning_pipeline(lrs=[0.0001],
     #                                   loss='BCE')
 
-    run_combined_evaluating_pipeline(test=True,
+    run_combined_evaluating_pipeline(test=False,
                                      lrs=[0.0001],
                                      loss='BCE',
                                      pretrained_path='vit_b_16_lr0.0001_BCE.pth')
