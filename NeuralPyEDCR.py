cbccfa4f2acf1bdf07e45ae82704328be9bc510d
shaykrichelifrenn <skricheli2@gmail.com>
2024-04-02 18:53:33 -0700

    2.4 update

--- NeuralPyEDCR.py
+++ NeuralPyEDCR.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
                                     original_num_epochs=20,
                                     include_inconsistency_constraint=False,
                                     secondary_model_name='vit_l_16_BCE',
-                                    binary_models=data_preprocessing.fine_grain_classes_str[0:4],
+                                    binary_models=data_preprocessing.fine_grain_classes_str,
                                     # lower_predictions_indices=lower_predictions_indices,
 
                                     EDCR_num_epochs=EDCR_num_epochs,
