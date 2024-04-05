ad92b0168c636ccebe6d135f50bf41870b78fce0
shaykrichelifrenn <skricheli2@gmail.com>
2024-04-04 23:09:49 -0700

    4.4 update

--- NeuralPyEDCR.py
+++ NeuralPyEDCR.py
@@ -176,8 +176,8 @@ class NeuralPyEDCR(PyEDCR.EDCR):
 if __name__ == '__main__':
     epsilons = [0.2]
 
-    for EDCR_num_epochs in [4]:
-        for neural_num_epochs in [15]:
+    for EDCR_num_epochs in [1]:
+        for neural_num_epochs in [1]:
             # for lower_predictions_indices in [[2], [2, 3], [2, 3, 4]]:
             print('\n' + '#' * 100 + '\n' +
                   utils.blue_text(f'EDCR_num_epochs = {EDCR_num_epochs}, neural_num_epochs = {neural_num_epochs}'
