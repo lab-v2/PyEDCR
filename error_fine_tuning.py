054e142ac22666009a583bd281e2a78a2c29ca55
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-05-05 11:59:27 -0700

    fix bugs

--- error_fine_tuning.py
+++ error_fine_tuning.py
@@ -33,7 +33,8 @@ class Error_detection_model(PyEDCR.EDCR):
 
     def learn_error_binary_model(self,
                                  model_name: str,
-                                 lr: typing.Union[float, str]):
+                                 lr: typing.Union[float, str],
+                                 pretrained_path: str = None):
         preprocessor, fine_tuners, loaders, devices = backbone_pipeline.initiate(
             data_str=self.data_str,
             model_name=model_name,
@@ -41,7 +42,8 @@ class Error_detection_model(PyEDCR.EDCR):
             lr=lr,
             fine_predictions=self.get_predictions(test=False, g=self.preprocessor.granularities['fine']),
             coarse_predictions=self.get_predictions(test=False, g=self.preprocessor.granularities['coarse']),
-            train_eval_split=0.8
+            train_eval_split=0.8,
+            pretrained_path=pretrained_path
         )
 
         combined_fine_tuning.fine_tune_combined_model(
@@ -61,15 +63,16 @@ class Error_detection_model(PyEDCR.EDCR):
 
 
 if __name__ == '__main__':
-    # data_str = 'military_vehicles'
-    # main_model_name = new_model_name = 'vit_b_16'
-    # main_lr = new_lr = binary_lr = 0.0001
-    # original_num_epochs = 20
+    data_str = 'military_vehicles'
+    main_model_name = new_model_name = 'vit_b_16'
+    main_lr = new_lr = binary_lr = 0.0001
+    original_num_epochs = 20
+    pretrained_path = ''
 
-    data_str = 'imagenet'
-    main_model_name = new_model_name = 'dinov2_vits14'
-    main_lr = new_lr = binary_lr = 0.000001
-    original_num_epochs = 8
+    # data_str = 'imagenet'
+    # main_model_name = new_model_name = 'dinov2_vits14'
+    # main_lr = new_lr = binary_lr = 0.000001
+    # original_num_epochs = 8
 
     # data_str = 'openimage'
     # main_model_name = new_model_name = 'tresnet_m'
@@ -85,4 +88,5 @@ if __name__ == '__main__':
                                  )
 
     edcr.learn_error_binary_model(model_name=main_model_name,
-                                  lr=new_lr)
+                                  lr=new_lr,
+                                  pretrained_path=pretrained_path)
