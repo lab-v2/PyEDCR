58673958a547867f21698e94371cadc5e21c9b85
shaykrichelifrenn <skricheli2@gmail.com>
2024-05-07 21:59:50 -0700

    7.5 update

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -302,8 +302,7 @@ class DataPreprocessor:
         if data_str == 'imagenet':
             data_path_str = 'data/ImageNet100/'
         elif data_str == 'openimage':
-            data_path_str = 'scratch/ngocbach/OpenImage/' if not config.running_on_sol \
-                else '/scratch/ngocbach/OpenImage/'
+            data_path_str = '~/../../scratch/ngocbach/OpenImage/'
         else:
             data_path_str = 'data/'
 
